8f8528f9d2850250b3612578754371e3b0a7b941
anais <anais.bruno26@gmail.com>
2021-10-15 12:54:37 +0200

    Classes Carte

--- src/main/java/model/Carte.java
+++ src/main/java/model/Carte.java
@@ -1,5 +1,56 @@
 package model;
 
-public class Carte {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+public abstract class Carte {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	protected int id;
+	protected String nom;
+	
+	public Carte(String nom) {
+		
+		this.nom = nom;
+	}
+
+	public Carte() {
+	
+	}
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Carte [id=" + id + ", nom=" + nom + "]";
+	}
+	
+	
+
+	
+	
+	
 }
