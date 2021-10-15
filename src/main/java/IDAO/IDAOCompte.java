ff1f5ed3fd551852ad4a5032a80e58a61004b88f
Biscay <hugo.bcpst@gmail.com>
2021-10-15 12:54:32 +0200

    Connec

--- src/main/java/IDAO/IDAOCompte.java
+++ src/main/java/IDAO/IDAOCompte.java
@@ -2,6 +2,7 @@ package IDAO;
 
 import model.Compte;
 
-public interface IDAOCompte extends IDAO<Compte,Integer> {
 
+public interface IDAOCompte extends IDAO<Compte,Integer> {
+	public Compte connect(String login,String password);
 }
